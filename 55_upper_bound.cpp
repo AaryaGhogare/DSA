b9d703d91b094f79043b6b73bfe3d7c3dd315a09
AaryaGhogare <ghogareaarya1@gmail.com>
2026-07-31 09:31:05 +0530

    Upper Bound

---
+++ 55_upper_bound.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution 
+{
+  public:
+      int lowerBound(vector<int> &nums, int x) 
+      {
+        int low,up,mid,n=nums.size();
+        low=0;
+        up=n-1;
+        while(low<=up)
+        {
+          mid=(low+up)/2;
+          if(nums[mid] <= x)
+          {
+          	low=mid+1;
+          }
+          else
+          {
+            up=mid-1;
+            if(nums[mid]>x)
+            {
+            	return mid;
+			}
+          }
+        }
+        return nums.size();//question demands-->If no such index is found, return the size of the array.
+      }
+};
+int main()
+{
+  vector <int> num;
+  int n,x,i;
+	vector <int> arr;
+  int ans;
+	cout<<"\nEnter the number of elements to add: ";
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cout<<"\nEnter a number: ";
+		cin>>x;
+		arr.push_back(x);
+	}
+  cout<<"\nEnter value x: ";
+  cin>>x;
+	Solution sol;
+	ans=sol.lowerBound(arr,x);
+	cout<<"\nThe index is: "<<ans;
+}
